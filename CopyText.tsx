fc18e1ec71b249afec1d450f9a326e728060cb82
Nayan Gajera <nayangajera.devoteeinfotech@gmail.com>
2024-05-27 14:01:42 +0530

    Optimize code

--- CopyText.tsx
+++ CopyText.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { ICopyText } from './copyText.model';
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { ICopyText } from "./copyText.model";
 
 const CopyText = (props: ICopyText) => {
-    const { text } = props;
-    const { t } = useTranslation();
-    const [copied, setCopied] = useState(false);
+  const { text } = props;
+  const { t } = useTranslation();
+  const [copied, setCopied] = useState(false);
 
-    return copied ? (
-        <i className="pi pi-check text-color ml-1"></i>
-    ) : (
-        <i
-            className="pi pi-copy ml-1 text-color ml-1 cursor-pointer"
-            onClick={() => {
-                navigator.clipboard.writeText(text);
-                setCopied(true);
-                setTimeout(() => setCopied(false), 1000); // Reset copied state after 1 seconds
-            }}
-            title={t('common.globalSearch.copyToClipboard') || ''}
-        ></i>
-    );
+  return copied ? (
+    <i className="pi pi-check text-color ml-1"></i>
+  ) : (
+    <i
+      className="pi pi-copy ml-1 text-color ml-1 cursor-pointer"
+      onClick={() => {
+        navigator.clipboard.writeText(text);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1000); // Reset copied state after 1 seconds
+      }}
+      title={t("common.globalSearch.copyToClipboard") || ""}
+    ></i>
+  );
 };
 
 export default CopyText;
